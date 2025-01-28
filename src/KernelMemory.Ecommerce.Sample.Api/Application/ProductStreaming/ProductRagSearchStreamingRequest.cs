babef81347cd1e2b674215920d8d0d7d344893c8
demid-ns <46938257+demid-ns@users.noreply.github.com>
2025-01-28 21:43:18 +0200

    Add product rag search with streaming

---
+++ src/KernelMemory.Ecommerce.Sample.Api/Application/ProductStreaming/ProductRagSearchStreamingRequest.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.KernelMemory;
+using KernelMemory.Ecommerce.Sample.Api.Application.Configuration;
+using Microsoft.Extensions.Options;
+using System.Runtime.CompilerServices;
+
+namespace KernelMemory.Ecommerce.Sample.Api.Application.ProductStreaming;
+
+public sealed record ProductRagSearchStreamRequest(string SearchQuery) : IStreamRequest<string>;
+
+public class ProductRagSearchStreamingRequestHandler(IKernelMemory memory, IOptions<ProductSearchOptions> options)
+    : IStreamRequestHandler<ProductRagSearchStreamRequest, string>
+{
+    public async IAsyncEnumerable<string> Handle(
+        ProductRagSearchStreamRequest request,
+        [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        var answerStream = memory.AskStreamingAsync(
+            request.SearchQuery,
+            minRelevance: options.Value.MinSearchResultsRelevance,
+            options: new SearchOptions { Stream = true },
+            cancellationToken: cancellationToken);
+
+        await foreach (var answer in answerStream)
+        {
+            yield return answer.Result;
+        }
+    }
+}
---
+++ src/KernelMemory.Ecommerce.Sample.Api/Presentation/ProductRagStreamingSearch.cs
@@ -0,0 +1,29 @@
+﻿using KernelMemory.Ecommerce.Sample.Api.Application.ProductStreaming;
+using MediatR;
+
+namespace KernelMemory.Ecommerce.Sample.Api.Presentation;
+
+public class ProductRagStreamingSearch : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/products/search/rag/streaming", async (
+            string searchQuery,
+            ISender sender,
+            HttpResponse response,
+            CancellationToken cancellationToken) =>
+        {
+            response.ContentType = "text/event-stream";
+
+            var request = new ProductRagSearchStreamRequest(searchQuery);
+
+            var resultStream = sender.CreateStream(request, cancellationToken);
+
+            await foreach (var result in resultStream)
+            {
+                await response.WriteAsync(result, cancellationToken);
+                await response.Body.FlushAsync(cancellationToken);
+            }
+        });
+    }
+}
